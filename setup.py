aafab5c07cf1a158bdec6f255a2060796a23a9a7
Alisson Perez <alissonperez@gmail.com>
2014-11-18 00:57:05 -0200

    Updated setup.py parameters

--- setup.py
+++ setup.py
@@ -17,21 +17,30 @@ setup(
     version=version,
     author="Alisson R. Perez",
     author_email="alissonperez@gmail.com",
-    # @todo - Update with our PyPI package page
+    # @todo - Update it with our PyPI package page
     url="https://pypi.python.org/pypi",
     packages=packages,
-    # scripts=["bin/loremdb"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Development Status :: 1 - Planning",
-        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "Topic :: Database",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Monitoring",
+        "Topic :: Utilities",
     ],
     install_requires=[
         "tweepy>=2.3",
     ],
-    # @todo - Include: "description" and "long_description"
+    description="Small library to create monitoring routines with Twitter DM",
+    long_description=("A small open source library to create monitoring "
+                      "routines of any nature using direct messages (DM) of "
+                      "Twitter. For each message send request, the library "
+                      "will take all the followers of configured account "
+                      "and send instantly a DM to each one.")
 )
